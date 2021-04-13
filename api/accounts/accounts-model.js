96f8e245d9ff978d025755cc1fdb187073aeb9cf
Stone98 <stone18003@gamil.com>
2021-04-13 16:46:12 -0700

    typo

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -5,7 +5,7 @@ const getAll = () => {
 };
 
 const getById = (id) => {
-  return db("accounts").where({ id }).first(); // returns the project who's id matched the id given firrst
+  return db("accounts").where({ id }).first(); // returns the project who's id matched the id given first
 };
 
 const create = async (account) => {
